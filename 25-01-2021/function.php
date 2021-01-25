c79b445f138815e4f1e67148309dca338271c3f7
Kruti Panchal <kbpanchal34@gmail.com>
2021-01-25 21:42:57 +0530

    Add files via upload

---
+++ 25-01-2021/function.php
@@ -0,0 +1,16 @@
+<?php
+
+    function name(){
+        return ' Kruti<br>';
+    }
+
+    echo 'My name is' .name();
+    
+    $num1 =5;
+    $num2 =10;
+    function addition($num1,$num2){
+        return $num1+$num2;
+    }
+
+    echo 'Sum is: ' .addition($num1,$num2);
+?>
